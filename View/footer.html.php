8894bd81a07e15a0b827beb2f7824559a066e80f
rossoromain <bif.rosso@gmail.com>
2021-12-10 16:31:12 +0100

    footer

--- View/footer.html.php
+++ View/footer.html.php
@@ -1,4 +1,72 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
+  <!-- Footer -->
+<footer class="text-center fixed-bottom text-lg-start bg-light text-muted">
+  
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">        
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            USERS
+          </h6>
+          <p>
+            <a href="index.php?ctrl=user&method=list" class="text-reset">List</a>
+          </p>
+          <p>
+            <a href="index.php?ctrl=user&method=create" class="text-reset">Create</a>
+          </p>
+          <p>
+            <a href="index.php?ctrl=user&method=list" class="text-reset">Edit</a>
+          </p>
+          <p>
+          <a href="index.php?ctrl=user&method=list" class="text-reset">Delete</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            POSTS
+          </h6>
+          <p>
+            <a href="index.php?ctrl=post&method=list" class="text-reset">List</a>
+          </p>
+          <p>
+            <a href="index.php?ctrl=post&method=create" class="text-reset">Create</a>
+          </p>
+          <p>
+            <a href="index.php?ctrl=post&method=list" class="text-reset">Edit</a>
+          </p>
+          <p>
+          <a href="index.php?ctrl=post&method=list" class="text-reset">Delete</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </html>
