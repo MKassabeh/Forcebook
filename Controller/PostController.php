35e907ce962b0c9873902fd7f20222a5d094ec04
rossoromain <bif.rosso@gmail.com>
2021-12-10 16:25:08 +0100

    architecture de post finie

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -2,24 +2,28 @@
 
 class PostController extends Controller
 {
-	public function list()
-	{
+	public function list()	{
+		
+		$this->render('post/list.html.php');
 		echo 'LISTE POSTS';
 	}
 
 	public function create()
 	{
+		$this->render('post/create.html.php');
 		echo 'CREATE POST';
 	}
 
 	public function edit()
 	{
+		$this->render('post/edit.html.php');
 		echo 'EDIT POST';
 	}
 
 
 	public function delete()
 	{
+		$this->render('post/list.html.php');
 		echo 'DELETE POST';
 	}
 }
