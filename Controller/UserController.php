19af286e644dac076e23289426d323d4df4196ba
rossoromain <bif.rosso@gmail.com>
2021-12-10 16:45:27 +0100

    Version finale de vendredi

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -1,7 +1,9 @@
 <?php
 
-class UserController extends Controller {
-	public function list() {
+class UserController extends Controller
+{
+	public function list()
+	{
 		//Je fais appel à mon manager
 		$userManager = new UserManager();
 
@@ -12,10 +14,10 @@ class UserController extends Controller {
 		$this->render('user/list.html.php', [
 			'users' => $users
 		]);
-		
 	}
 
-	public function create() {
+	public function create()
+	{
 		$errors = [];
 		$form_sent = false;
 		$safe = array_map('trim', array_map('strip_tags', $_POST));
@@ -38,34 +40,56 @@ class UserController extends Controller {
 				// instance du manager
 				$userManager = new UserManager();
 				// création de l'objet contenant les informations pour l'insertion en bdd
-				$user = new User();				
+				$user = new User();
 				$user->setName($safe['name'])->setEmail($safe['email']);
-				$userManager->create($user);		
-			// echec
-			} else {
-				
+				$userManager->create($user);
+			}
+			// premier affichage
+		}
+
+		$this->render('user/create.html.php', ['errors' => $errors, 'form_sent' => $form_sent]);
+	}
+
+	public function edit()
+	{
+
+		// instance du manager
+		$userManager = new UserManager();
+		// création de l'objet contenant les informations pour l'insertion en bdd
+		$user = $userManager->find($_GET['user']);
+
+		$errors = [];
+		$form_sent = false;
+		$safe = array_map('trim', array_map('strip_tags', $_POST));
+		//formulaire soumis
+		if (isset($_POST['name']) && isset($_POST['email'])) {
+			$form_sent = true;
+			if (strlen($safe['name']) <= 0) {
+				$errors[] = 'Veuillez saisir un nom';
+			}
+
+			if (strlen($safe['email']) <= 0) {
+				$errors[] = 'Veuillez saisir un email';
+			}
+
+			if ((!filter_var($safe['email'], FILTER_VALIDATE_EMAIL))) {
+				$errors[] = 'Veuillez saisir un email au format valide';
+			}
+			// succes
+			if (count($errors) == 0) {
+
+				$user->setName($safe['name'])->setEmail($safe['email']);
+				$userManager->update($user);
+				// echec
 			}
 		}
-		// premier affichage
-		
-		$this->render('user/create.html.php', ['errors' => $errors, 'form_sent' => $form_sent]);
-
-	}
-
-	public function edit() {
-
-		$userManager = new UserManager();
-
-		$user = $userManager-> find($_GET['user']);
-
-		$this->render('user/edit.html.php', [
-			'user' => $user
-		]);
+		$this->render('user/edit.html.php', ['errors' => $errors, 'form_sent' => $form_sent, 'user' => $user]);
 	}
 
 
-	public function delete() {
-		
+	public function delete()
+	{
+
 		// Select puis delete
 		$userManager = new UserManager();
 
@@ -75,7 +99,7 @@ class UserController extends Controller {
 
 		header('Location: index.php?ctrl=user&method=list'); // renvoi vers la page d'accueil
 
-		
+
 
 	}
 }
--- View/user/edit.html.php
+++ View/user/edit.html.php
@@ -14,12 +14,12 @@
             <form method="POST">
             <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" name="name" id="name" placeholder="<?=$user->getName()?>">
+                <input type="text" class="form-control" name="name" id="name" placeholder="<?=$user->getName();?>">
                 </div>
             
                 <div class="mb-3">
                 <label for="email" class="form-label">New Email</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="<?=$user->getEmail()?>">
+                <input type="email" class="form-control" name="email" id="email" placeholder="<?=$user->getEmail();?>">
                 </div>
             
                 <div class="mb-3">
@@ -30,3 +30,7 @@
         </div>
     </div>
 </div>
+
+<?php if (!empty($errors)){ echo '<div class="alert alert-danger">'.implode("<br>", $errors).'</div>';} ?>
+
+<?php if ($form_sent && empty($errors)){echo '<div class="alert alert-success"> Utilisateur modifié ! </div>';}?>
