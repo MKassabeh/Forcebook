f1cd78cdb0e35c22fceddcd438a277f1b042e5ae
rossoromain <bif.rosso@gmail.com>
2021-12-10 11:08:59 +0100

    ctrl=user&method=create

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -12,11 +12,44 @@ class UserController extends Controller {
 		$this->render('user/list.html.php', [
 			'users' => $users
 		]);
-		$this->render('user/create.html.php');
+		
 	}
 
 	public function create() {
-		echo "CREATE";
+		$errors = [];
+		$form_sent = false;
+		$safe = array_map('trim', array_map('strip_tags', $_POST));
+		//formulaire soumis
+		if (isset($_POST['name']) && isset($_POST['email'])) {
+			$form_sent = true;
+			if (strlen($safe['name']) <= 0) {
+				$errors[] = 'Veuillez saisir un nom';
+			}
+
+			if (strlen($safe['email']) <= 0) {
+				$errors[] = 'Veuillez saisir un email';
+			}
+
+			if ((!filter_var($safe['email'], FILTER_VALIDATE_EMAIL))) {
+				$errors[] = 'Veuillez saisir un email au format valide';
+			}
+			// succes
+			if (count($errors) == 0) {
+				// instance du manager
+				$userManager = new UserManager();
+				// création de l'objet contenant les informations pour l'insertion en bdd
+				$user = new User();				
+				$user->setName($safe['name'])->setEmail($safe['email']);
+				$userManager->create($user);		
+			// echec
+			} else {
+				
+			}
+		}
+		// premier affichage
+		
+		$this->render('user/create.html.php', ['errors' => $errors, 'form_sent' => $form_sent]);
+
 	}
 
 	public function edit() {
--- View/user/create.html.php
+++ View/user/create.html.php
@@ -2,18 +2,26 @@
     <h1>User management</h1>
 
     <div class="card">
-        <div class="card-body">
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="email@example.fr">
+        <form method="POST" novalidate>
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="email@example.fr">
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="User name">
+                </div>
+                <div class="text-center mb-3">
+                    <input type="submit" value="Save" class="btn btn-success">
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="User name">
-            </div>
-            <div class="text-center mb-3">
-                <input type="submit" value="Save" class="btn btn-success">
-            </div>
-        </div>
+        </form>
     </div>
 </div>
+
+
+<?php if (!empty($errors)){ echo '<div class="alert alert-danger">'.implode("<br>", $errors).'</div>';} ?>
+
+<?php if ($form_sent && empty($errors)){echo '<div class="alert alert-success"> Utilisateur ajouté ! </div>';}?>
+      
